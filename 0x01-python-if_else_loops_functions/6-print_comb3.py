db75b2f7c877807c686b30e6618d86b84e526635
root <gikarocaleb14@gmail.com>
2022-05-31 14:03:50 -0700

    File 6 update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 99:
-        print("{:02d}".format(i), end=', ')
-    else:
-        print("{:02d}".format(i), end='\n')
+    for j in range(i, 10):
+        if i != j:
+            if i < 8:
+                print("{:d}{:d}".format(i, j), end=', ')
+            else:
+                print("{:d}{:d}".format(i, j), end='\n')
