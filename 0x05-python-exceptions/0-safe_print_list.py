d71214d6533acfcf95d6ed0a81b5fc61ab570bac
root <gikarocaleb14@gmail.com>
2022-06-20 07:27:57 -0700

    Integers division with debug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    number = 0
+    count = 0
     for n in range(0, x):
         try:
             print('{}'.format(my_list[n]), end='')
-            number += 1
+            count += 1
         except:
             pass
         print()
-        return number
+        return count
