33ac72998b86334b42cd4d356037815b44466aa1
root <gikarocaleb14@gmail.com>
2022-06-20 08:47:47 -0700

    update of file 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """
-    Prints integer with format flags
-    """
     try:
-        print('{:d}'.format(value))
+        print("{:d}".format(value))
+        return True
     except:
         return False
-    return True
