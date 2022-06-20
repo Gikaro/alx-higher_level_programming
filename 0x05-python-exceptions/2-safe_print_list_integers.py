c8240698521544476445ddb5939fba1791af7761
root <gikarocaleb14@gmail.com>
2022-06-20 08:52:46 -0700

    update of file 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """
-    Prints the first x elements of a list and onlu intger
-    """
-    number = 0
-    for i in range(0, x):
+    filter = 0
+    for i in range(1, 3):
         try:
-            print('{:d}'.format(my_list[i]), end='')
-            number += 1
-        except (ValueError, TypeError):
-            pass
-        print()
-        return number
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+            except:
+                result = b + a
+                break
+        return result
