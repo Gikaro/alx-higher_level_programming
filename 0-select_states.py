c009555ea3b99b6688ffc4be85dd34961573e541
Gikaro <gikarocaleb14@gmail.com>
2022-09-11 21:28:42 +0300

    update of file1

--- 0-select_states.py
+++ 0-select_states.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-"""
-SelectStates module
-"""
-import MySQLdb
-import sys
+"""lists all states from the database hbtn_0e_0_usa"""
 
+if __name__ == '__main__':
 
-def select_state():
-    """Grabs state from database"""
+    import MySQLdb
+    import sys
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', port=3306,
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    db = MySQLdb.connect(host='localhost',
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-            )
     cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY id ASC')
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
-
-if __name__ == "__main__":
-    select_states()
